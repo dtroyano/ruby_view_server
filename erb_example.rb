7070d27c5900f352e05b3f17fda505eb38a8cc0f
Daniel Troyano <dtroyano@cox.net>
2013-01-08 15:04:53 -0600

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name = "bob"
+template = ERB.new "Hello <%= name %>. <%= name.upcase %>!"
+puts template.result(binding)
+
+numbers = [3,2,4]
+template = ERB.new "The answer is <%= numbers[0]+numbers[1]+numbers[2] %>"
+puts template.result(binding)
